38e240b21ff263ee90cfb2d9f3abec1a558f4753
flash <k.kostov.eu@gmail.com>
2017-05-07 18:41:48 +0300

    setup question page, setup questions routing

--- public/js/controllers/questionController.js
+++ public/js/controllers/questionController.js
@@ -1,13 +1,40 @@
 'use strict';
 
+import dataprovider from 'dataProvider';
 
 class QuestionController {
 
     load(parameters) {
 
-        let $appContainer = $('#app-container');
+        let $APP_CONTAINER = $('#app-container');
 
-        $appContainer.html(`QUESTION ${parameters.id}`);
+        let questionId = Number(parameters.id);
+
+        // get all questions
+
+        const ALL_QUESTIONS_URL = 'https://bansheeproject-7bb5a.firebaseio.com/.json';
+
+        Promise.all([dataprovider.getQuestions(ALL_QUESTIONS_URL), dataprovider.getTemplate('question')])
+            .then(function ([data, renderer]) {
+
+                let cSharpArray = data['c-sharp-questions'];
+                let jsArray = data['javascript-questions'];
+
+                let allQuestions = cSharpArray.concat(jsArray);
+
+                console.log(allQuestions);
+
+                let questionToDisplay = allQuestions.find(x => x.id === questionId);
+
+                console.log(questionToDisplay);
+
+                let template = renderer(questionToDisplay);
+
+                $APP_CONTAINER.html(template);
+
+            }).catch(function () {
+                console.log('Error: homecontroller promise catch');
+            });
 
     }
 }
